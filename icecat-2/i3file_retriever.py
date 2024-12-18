c43d0c9c42623159e4a7a59aa1b28f809f779c4a
G-Sommani <giacomo.sommani@gmail.com>
2024-12-18 07:19:28 -0600

    include skymist project

---
+++ icecat-2/i3file_retriever.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append("/home/gsommani/newskymist/src/")
+from datetime import timedelta
+import base64
+import pickle
+import zlib
+
+from icecube import dataio
+
+from skymist import i3live
+
+def retrieve_i3file(
+    run_id: int, event_id: int, output: str = "",
+) -> None:
+
+    live_skymist = i3live.I3Live()
+    events_run = live_skymist.events_from_run(run_number=run_id)
+    n_ev = len(events_run)
+    if n_ev == 0:
+        raise RuntimeError(
+            f"I3Live has no events for {run_id}. Either the run number is wrong or it predates the inception of the realtime system."
+        )
+    
+    first_evt = events_run[0]["value"]["data"]["event_id"]
+    last_evt = events_run[-1]["value"]["data"]["event_id"]
+    
+    for event in events_run:
+        pending_event_id = int(event["value"]["data"]["event_id"])
+        #print('pending_event_id: ', pending_event_id)
+        #print('event_id: ', event_id)
+        #print('Time: ', event["time"])
+        if pending_event_id == event_id:
+            timestamp = event["time"]
+            try:
+                start = live_skymist.parse_date(timestamp) - timedelta(seconds=30)
+                stop = live_skymist.parse_date(timestamp) + timedelta(seconds=30)
+                print('Time between '+str(start)+' and '+str(stop))
+            except ValueError as e:
+                LOGGER.error(e)
+    
+    events = live_skymist.get_records(start, stop, with_data=True)
+    print('I3Live returned {:d} events.'.format(len(events)))
+    for event in events:
+        if event["value"]["data"]["event_id"] == event_id:
+            # write frames to .i3 file
+            i3file = dataio.I3File(output, 'w')
+            text_frames = event['value']['data']['frames']
+            for frame_type, frame_content in text_frames:
+                frame = pickle.loads(zlib.decompress(base64.b64decode(frame_content)),
+                                     encoding='bytes')
+            # for frame in i3_frames:
+                i3file.push(frame)
+            i3file.close()
+            print('Wrote', output)
