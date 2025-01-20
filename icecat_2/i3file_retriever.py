7d4673e6bd3abdd1bfe4a7b3592ad8b32dc9d0d3
Giacomo Sommani <79135283+G-Sommani@users.noreply.github.com>
2025-01-20 17:36:46 +0100

    correct handling of exception

--- icecat_2/i3file_retriever.py
+++ icecat_2/i3file_retriever.py
@@ -159,11 +159,18 @@ def retrieve_old_i3file(
                     frame.Delete(key_in_frame)
                 elif key_in_frame not in cfg.possible_keys:
                     frame.Delete(key_in_frame)
-                elif key_in_frame not in inserted_keys:
-                    if new_key_in_frame == "SplitInIceDSTPulses":
-                        new_key_in_frame = "SplitUncleanedInIcePulses"
-                    if new_key_in_frame == "SplitInIceDSTPulsesTimeRange":
-                        new_key_in_frame = "SplitUncleanedInIcePulsesTimeRange"
+                elif key_in_frame not in inserted_keys:                    
+                    if key_in_frame == "SplitInIceDSTPulses":
+                        element = frame.Get(key_in_frame)
+                        frame.Delete(key_in_frame)
+                        key_in_frame = "SplitUncleanedInIcePulses"
+                    elif key_in_frame == "SplitInIceDSTPulsesTimeRange":
+                        element = frame.Get(key_in_frame)
+                        frame.Delete(key_in_frame)
+                        key_in_frame = "SplitUncleanedInIcePulsesTimeRange"
+                    else:
+                        element = frame.Get(key_in_frame)
+                        frame.Delete(key_in_frame)
                     element = frame.Get(key_in_frame)
                     frame.Delete(key_in_frame)
                     frame.Put(
