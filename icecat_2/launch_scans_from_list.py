0b2c4956beeb8f42f37de827c5fee6875393f71a
Angela Zegarelli <azegarelli@cobalt-16.icecube.wisc.edu>
2025-03-06 08:18:29 -0600

    change frame to be checked (where alert stream is saved) and gives as input i3 files including gcd pass2 files

--- icecat_2/launch_scans_from_list.py
+++ icecat_2/launch_scans_from_list.py
@@ -1,8 +1,9 @@
+import time
 from pathlib import Path
 import numpy as np
 from icecube import dataio, icetray
 from skymist.skyscan import SkyScan
-
+import os
 import config
 cfg = config.config()
 
@@ -12,13 +13,13 @@ def check_stream(infile):
     is_HED = False
     for frame in i3file:
         if frame.Stop == icetray.I3Frame.Physics:
-            if frame.Has(cfg.key_passedfilters):
-                streams = frame.Get(cfg.key_passedfilters)
+            if frame.Has("AlertNamesPassed_IceCat2"):
+                streams = frame.Get("AlertNamesPassed_IceCat2")
                 print(streams)
                 if "HESE" in streams:
                     is_HED = True
             else:
-                raise ValueError(f"The i3 file has not {cfg.key_passedfilters} in it!")
+                raise ValueError("The i3 file has not AlertNamesPassed_IceCat2 in it!")
             if frame.Has("OnlineL2_SplineMPE_TruncatedEnergy_ORIG_Muon"):
                 te_orig = frame.Get("OnlineL2_SplineMPE_TruncatedEnergy_ORIG_Muon")
                 te_orig_value = te_orig.energy
@@ -39,7 +40,7 @@ def launch_reco(filepath):
         njobs = 100
     else:
         reco = "splinempe"
-        njobs = 10
+        njobs = 100
 
     print(f"Event is HED: {is_HED}")
     print(f"Reco: {reco}, njobs: {njobs}")
@@ -54,35 +55,33 @@ def launch_reco(filepath):
         "fine",
         njobs,
         "latest",
-        additional_tag="icecat_test"
+        additional_tag="icecat_gdcpass2"
     )
 
-'''
+
 print('Old events')
-infile = cfg.alerts_table_dir+"alerts_no_i3live.txt"
-run, eventid = np.loadtxt(infile, usecols=(0,1), unpack=True, dtype=int)
+infile = cfg.alerts_table_dir+"alerts_no_i3live.csv"
+run, eventid = np.loadtxt(infile, usecols=(0,1), unpack=True, dtype=int, delimiter=',')
 for i in range(len(run)):
     print(f"Run: {run[i]}, EvtID: {eventid[i]}")
-    filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_old_te.i3'
+    filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_old_pass2_te.i3'
+    #filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_old_te.i3'
     filepath = cfg.i3files_dir + filename
+    print(filepath)
     launch_reco(filepath)
+    time.sleep(10)
 
 print('Events from I3Live ...')
-infile = cfg.alerts_table_dir+"alerts_i3live.txt"
-run, eventid = np.loadtxt(infile, usecols=(0,1), unpack=True, dtype=int)
+infile = cfg.alerts_table_dir+"alerts_i3live.csv"
+run, eventid = np.loadtxt(infile, usecols=(0,1), unpack=True, dtype=int, delimiter=',')
 for i in range(len(run)):
-    print(f"Run: {run[i]}, EvtID: {eventid[i]}")
-    filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_i3live_te.i3'
-    filepath = cfg.i3files_dir + filename
-    launch_reco(filepath)
-'''
-
-infile = cfg.alerts_table_dir+"last_alerts.txt"
-run, eventid, te_orig, evt_type, stream = np.loadtxt(infile, usecols=(0,1,2,3,4), unpack=True, dtype=str)
-for i in range(len(run)):
-    print(f"Run: {run[i]}, EvtID: {eventid[i]}")
-    print(f"TE ORIG [TeV]: {te_orig[i]} --> evt_type: {evt_type[i]}")
-    print(f"stream: {stream[i]}")
-    filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_i3live_te.i3'
-    filepath = cfg.i3files_dir + filename
-    launch_reco(filepath)
+    check_run = 0
+    for j in range(len(run_last)):
+        if(run[i]==run_last[j]): check_run = 1
+    if(check_run==0):
+        print(f"Run: {run[i]}, EvtID: {eventid[i]}")
+        filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_i3live_pass2_te.i3'
+        #filename='run'+str(run[i])+'_eventid'+str(eventid[i])+'_i3live_te.i3'
+        filepath = cfg.i3files_dir + filename
+        launch_reco(filepath)
+    time.sleep(10)
